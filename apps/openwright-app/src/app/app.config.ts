75d828ac47495842b4d71ea83bc462e78a2c5ac9
Martin Zima <mail@zimamartin.cz>
2025-04-05 07:44:39 +0200

    added primeng and tailwind

--- apps/openwright-app/src/app/app.config.ts
+++ apps/openwright-app/src/app/app.config.ts
@@ -5,11 +5,20 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    })
   ],
 };
